6567532e0e07750cb089214fabce4fec0dfdd416
Lee Angelina <angelinalee_3110@mail.ru>
2023-10-01 22:21:22 +0600

    update hw3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { Component, useState} from 'react';
 
-// Higher Order Component (HOC)
-function withLogger(WrappedComponent) {
-  return class extends React.Component {
-    componentDidMount() {
-      console.log(`Component ${WrappedComponent.name} mounted.`);
-    }
+class Counter extends Component {
+  componentDidMount() {
+    document.title = `Count: ${this.props.count}`;
+  }
 
-    componentWillUnmount() {
-      console.log(`Component ${WrappedComponent.name} will unmount.`);
+  componentDidUpdate(prevProps) {
+    if (prevProps.count !== this.props.count) {
+      document.title = `Count: ${this.props.count}`;
     }
+  }
 
-    render() {
-      return <WrappedComponent {...this.props} />;
-    }
-  };
+  render() {
+    return (
+      <div>
+        <p>Count: {this.props.count}</p>
+        <button onClick={this.props.increment}>Increment</button>
+      </div>
+    );
+  }
 }
-
-// Функциональный компонент, использующий Hook
-function Counter({ count, increment }) {
-  useEffect(() => {
-    document.title = `Count: ${count}`;
-  }, [count]);
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-    </div>
-  );
-}
-
-// Компонент, обернутый в HOC
-const WrappedCounter = withLogger(Counter);
 
 function App() {
   const [count, setCount] = useState(0);
@@ -44,10 +31,11 @@ function App() {
 
   return (
     <div>
-      <h1>React App with Hooks, HOC, and Lifecycle Methods</h1>
-      <WrappedCounter count={count} increment={increment} />
+      <h1>React App with Lifecycle Methods</h1>
+      <Counter count={count} increment={increment} />
     </div>
   );
 }
 
 export default App;
+
