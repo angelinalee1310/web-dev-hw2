0c3075cb2fa091a1c919ac5c829309f8be48c93e
Lee Angelina <angelinalee_3110@mail.ru>
2023-10-01 22:13:06 +0600

    add hw3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+// Higher Order Component (HOC)
+function withLogger(WrappedComponent) {
+  return class extends React.Component {
+    componentDidMount() {
+      console.log(`Component ${WrappedComponent.name} mounted.`);
+    }
+
+    componentWillUnmount() {
+      console.log(`Component ${WrappedComponent.name} will unmount.`);
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+}
+
+// Функциональный компонент, использующий Hook
+function Counter({ count, increment }) {
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+// Компонент, обернутый в HOC
+const WrappedCounter = withLogger(Counter);
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React App with Hooks, HOC, and Lifecycle Methods</h1>
+      <WrappedCounter count={count} increment={increment} />
     </div>
   );
 }
