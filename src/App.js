7f4f9fbd280655a60d09f241fcdf49f899c5484c
Lee Angelina <angelinalee_3110@mail.ru>
2023-10-08 10:55:28 +0600

    add hw3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import React, { Component, useState} from 'react';
+import React, { Component, useState, useEffect, useCallback, useMemo } from 'react';
 
 class Counter extends Component {
   componentDidMount() {
@@ -25,17 +24,27 @@ class Counter extends Component {
 function App() {
   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+  const increment = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, []);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  // Memoized function to calculate a result based on count
+  const calculateResult = useMemo(() => {
+    // Expensive calculation based on count
+    return count * 2;
+  }, [count]);
 
   return (
     <div>
-      <h1>React App with Lifecycle Methods</h1>
+      <h1>React App with Event Handlers and Memoization</h1>
       <Counter count={count} increment={increment} />
+      <p>Calculated Result: {calculateResult}</p>
     </div>
   );
 }
 
 export default App;
-
